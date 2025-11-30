4d7a3ef261c68eca6dbdf79acdd1a3b359fefe46
SeanA <sean.ammon@mymail.champlain.edu>
2025-11-30 00:19:58 -0500

    Implemented Image Sorting with grids, and added some images

--- Image Sorter C++/raylib-cpm-cmake-boilerplate-main/main.cpp
+++ Image Sorter C++/raylib-cpm-cmake-boilerplate-main/main.cpp
@@ -9,7 +9,9 @@
 // Function Prototypes
 bool SimilarColors(const Color& refenceColor, const Color& checkingColor, int variance);
 void SortColorIntArrays(std::vector<Color>& colorVector, std::vector<int>& repeatVector);
-void SortByRepition(Color* imageData, int width, int height, int variance);
+void SortByRepetition(Color* imageData, int width, int height, int variance);
+void GridSort(Color* imageReturn, Image* imageFrom, Image* imageTo, int gridDivision);
+void ImageSort(Color* imageReturn, Image* imageFrom, Image* imageTo);
 
 int main()
 {
@@ -21,19 +23,21 @@ int main()
 
     SetTargetFPS(60);
 
-    Image imageFrom = LoadImage("Assets/red_to_green.png");
+    Image imageFrom = LoadImage("Assets/black_to_white.png");
+    Image imageTo = LoadImage("Assets/Obamify1.png");
     auto pixelsFrom = (Color*)imageFrom.data;
 
     const int imgFromWidth = imageFrom.width, imgFromHeight = imageFrom.height;
     Image imageResult = GenImageColor(imgFromWidth, imgFromHeight, WHITE);
     auto pixelsResult = pixelsFrom;
     Texture2D resultTexture = LoadTextureFromImage(imageResult);
-    Texture2D originalTexture = LoadTextureFromImage(imageFrom);
+    Texture2D fromTexture = LoadTextureFromImage(imageFrom);
+    Texture2D toTexture = LoadTextureFromImage(imageTo);
 
     // Performing the sort
-    int colorVariance = 4; // Between 0 and 255
-    SortByRepition(pixelsResult, imgFromWidth, imgFromHeight, colorVariance);
-
+    //int colorVariance = 4; // Between 0 and 255
+    //SortByRepetition(pixelsResult, imgFromWidth, imgFromHeight, colorVariance);
+    GridSort(pixelsResult, &imageFrom, &imageTo, 1);
 
     UpdateTexture(resultTexture, pixelsResult);
 
@@ -45,12 +49,14 @@ int main()
         // drawing logic goes here
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawTextureEx(originalTexture, Vector2(20, 20), 0, scaleFactor, WHITE);
-        DrawTextureEx(resultTexture, Vector2(40 + (imgFromWidth * scaleFactor), 20), 0, scaleFactor, WHITE);
+        DrawTextureEx(fromTexture, Vector2(20, 20), 0, scaleFactor, WHITE);
+        DrawTextureEx(toTexture, Vector2(40 + (imgFromWidth * scaleFactor), 20), 0, scaleFactor, WHITE);
+        DrawTextureEx(resultTexture, Vector2(40 + (imgFromHeight * scaleFactor) / 2, imgFromHeight * scaleFactor + 40), 0, scaleFactor, WHITE);
         EndDrawing();
     }
 
-    UnloadTexture(originalTexture);
+    UnloadTexture(fromTexture);
+    UnloadTexture(toTexture);
     UnloadTexture(resultTexture);
 
     CloseWindow();
@@ -87,7 +93,7 @@ void SortColorIntArrays(std::vector<Color>& colorVector, std::vector<int>& repea
         colorVector[checkIndex + 1] = currentColorValue;
     }
 }
-void SortByRepition(Color* imageData, int width, int height, int variance)
+void SortByRepetition(Color* imageData, int width, int height, int variance)
 {
     // Deifning Vectors
     std::vector<Color> usedColors;
@@ -135,5 +141,110 @@ void SortByRepition(Color* imageData, int width, int height, int variance)
             imageData[pixelIndex] = usedColors[i];
             pixelIndex++;
         }
+    }
+}
+void SortValueWithIndex(std::vector<float>& color, std::vector<int>& index)
+{
+    for (int i = 0; i < (color.size()); i++)
+    {
+        float currentColorValue = color[i];
+        int currentIndexValue = index[i];
+        int checkIndex = i - 1;
+
+        while (checkIndex >= 0 && color[checkIndex] > currentColorValue)
+        {
+            color[checkIndex + 1] = color[checkIndex];
+            index[checkIndex + 1] = index[checkIndex];
+            checkIndex--;
+        }
+        color[checkIndex + 1] = currentColorValue;
+        index[checkIndex + 1] = currentIndexValue;
+    }
+}
+void GridSort(Color* imageReturn, Image* imageFrom, Image* imageTo, int gridDivision)
+{
+    int gridWidth = (imageFrom->width > imageTo->width ? imageTo->width : imageFrom->width);
+    int gridHeight = (imageFrom->height > imageTo->height ? imageTo->height : imageFrom->height);
+
+    Color* fromData = (Color*)imageFrom->data;
+    Color* toData = (Color*)imageTo->data;
+
+    int gridSize = gridWidth / gridDivision;
+
+    // Looping through each grid
+    for (int gridY = 0; gridY < gridDivision; gridY++)
+    {
+        for (int gridX = 0; gridX < gridDivision; gridX++)
+        {
+            // Vectors for keeping track of values
+            std::vector<float> colorFromValues;
+            std::vector<int> colorFromIndex;
+            std::vector<float> colorToValues;
+            std::vector<int> colorToIndex;
+
+            // Looping through each pixel in the grid
+            for (int localY = 0; localY < gridSize; localY++)
+            {
+                for (int localX = 0; localX < gridSize; localX++)
+                {
+                    int x = gridX * gridSize + localX;
+                    int y = (localY * gridHeight) + (gridY * gridHeight * gridSize);
+                    int position = x + y;
+
+                    colorFromValues.push_back(0.299f * fromData[position].r + 0.587f * fromData[position].g + 0.114f * fromData[position].b);
+                    colorFromIndex.push_back(position);
+                    colorToValues.push_back(0.299f * toData[position].r + 0.587f * toData[position].g + 0.114f * toData[position].b);
+                    colorToIndex.push_back(position);
+                }
+            }
+
+            // Sorting Each Array
+            SortValueWithIndex(colorFromValues, colorFromIndex);
+            SortValueWithIndex(colorToValues, colorToIndex);
+
+            // Assigning the values based on the sorted array
+            for (int i = 0; i < colorFromValues.size(); i++)
+            {
+                imageReturn[colorToIndex[i]] = fromData[colorFromIndex[i]];
+            }
+        }
+    }
+}
+void ImageSort(Color* imageReturn, Image* imageFrom, Image* imageTo)
+{
+    int imageWidth = (imageFrom->width > imageTo->width ? imageTo->width : imageFrom->width);
+    int imageHeight = (imageFrom->height > imageTo->height ? imageTo->height : imageFrom->height);
+
+    Color* fromData = (Color*)imageFrom->data;
+    Color* toData = (Color*)imageTo->data;
+
+    // Vectors for keeping track of values
+    std::vector<float> colorFromValues;
+    std::vector<int> colorFromIndex;
+    std::vector<float> colorToValues;
+    std::vector<int> colorToIndex;
+
+    // Looping through each pixel
+    for (int localY = 0; localY < imageHeight; localY++)
+    {
+        for (int localX = 0; localX < imageWidth; localX++)
+        {
+            int position = localX + imageHeight * localY;
+
+            colorFromValues.push_back(0.299f * fromData[position].r + 0.587f * fromData[position].g + 0.114f * fromData[position].b);
+            colorFromIndex.push_back(position);
+            colorToValues.push_back(0.299f * toData[position].r + 0.587f * toData[position].g + 0.114f * toData[position].b);
+            colorToIndex.push_back(position);
+        }
+    }
+
+    // Sorting Each Array
+    SortValueWithIndex(colorFromValues, colorFromIndex);
+    SortValueWithIndex(colorToValues, colorToIndex);
+
+    // Assigning the values based on the sorted array
+    for (int i = 0; i < colorFromValues.size(); i++)
+    {
+        imageReturn[colorToIndex[i]] = fromData[colorFromIndex[i]];
     }
 }
